3dd2ba5b79b3c086649f1e3368062a1df9f1bb61
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-07 08:54:10 +0100

    Update zadanie_4.c
    wymiana ifa

--- 30.11.2019/zadanie_4.c
+++ 30.11.2019/zadanie_4.c
@@ -6,19 +6,23 @@ int koniec, wynik;
 void wpisz_liczbe()
 {
     printf("Wpisz liczbe calkowita większą od 0: ");
-    scanf("%d", koniec);
+    scanf("%d", &koniec);
 }
 
 int main()
 {
     wpisz_liczbe();
 
-    if(koniec<1){wpisz_liczbe();};
+    while(koniec<1)
+    {
+        printf("Wpisałeś niepoprawną liczbę.\n");
+        wpisz_liczbe();
+    };
 
     for(int i=0; i<koniec; i++)
     {
      wynik+=i;
     }
 
-    printf("Wynik to %d", wynik);
+    printf("Wynik to %d \n", wynik);
 }
