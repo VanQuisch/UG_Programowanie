f514c526a4abefafd1deb7804e0b0d849e7a0dbd
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-07 08:50:19 +0100

    Update zadanie_4.c
    Uzupełnienie kodu

--- 30.11.2019/zadanie_4.c
+++ 30.11.2019/zadanie_4.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int koniec, wynik;
+
+void wpisz_liczbe()
+{
+    printf("Wpisz liczbe calkowita większą od 0: ");
+    scanf("%d", koniec);
+}
+
 int main()
 {
-    printf("Wpisz liczbe calkowita: ");
-    //scanf(%d)
+    wpisz_liczbe();
+
+    if(koniec<1){wpisz_liczbe();};
+
+    for(int i=0; i<koniec; i++)
+    {
+     wynik+=i;
+    }
+
+    printf("Wynik to %d", wynik);
 }
