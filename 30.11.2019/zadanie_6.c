2d809fa0bb8886cdcfcf905f9431cdfb38f608d8
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-07 09:47:17 +0100

    Update zadanie_6.c
    update if:
    wywalono zbędnego IF

--- 30.11.2019/zadanie_6.c
+++ 30.11.2019/zadanie_6.c
@@ -8,21 +8,18 @@ int main()
 {
   printf("Wpisz 6 cyfr: \n");
 
-  for(int i=0; i<6; i++)  //wczytanie
+  for(int i=1; i<7; i++)  //wczytanie
     {
     scanf("%d", &tablica[i]);
     };
 
-  if(tablica[1]<tablica[2]){max=tablica[1];};
-  if(max<tablica[2]){max=tablica[2];};
+  if(tablica[1]>tablica[2]){max=tablica[1];}; //szukanie najwięksej liczby
   if(max<tablica[3]){max=tablica[3];};
   if(max<tablica[4]){max=tablica[4];};
   if(max<tablica[5]){max=tablica[5];};
   printf("Największa cyfra: %d \n", max);
 
-  min=tablica[5];
-  if(tablica[1]>tablica[2]){min=tablica[1];};
-  if(min>tablica[2]){min=tablica[2];};
+  if(tablica[1]>tablica[2]){min=tablica[1];}; //szukanie najmniejszej liczby
   if(min>tablica[3]){min=tablica[3];};
   if(min>tablica[4]){min=tablica[4];};
   if(min>tablica[5]){min=tablica[5];};
