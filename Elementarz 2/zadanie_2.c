685ecf0a6265c97589f7aa8c90317b84d8b75bd7
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-14 06:21:57 +0100

    Rename zadanie_1.c to zadanie_2.c

--- Elementarz 2/zadanie_2.c
+++ Elementarz 2/zadanie_2.c
@@ -5,12 +5,12 @@ int main() {
   int n;
   char jest_pierwsza[9];
   printf("Podaj liczbe calkowita: ");
-  /* wczytaj tê liczbê do zmiennej n */
+  /* wczytaj tÄ™ liczbÄ™ do zmiennej n */
   scanf("%d", &n);
-  /* sprawdŸ czy n jest liczb¹ pierwsz¹ */
+  /* sprawdÅº czy n jest liczbÄ… pierwszÄ… */
   if (n%2==0 && n!=2){jest_pierwsza[0] = "pierwsza";}
-  else {jest_pierwsza[0] = "z³o¿ona";};
+  else {jest_pierwsza[0] = "zÅ‚oÅ¼ona";};
   printf("Podana liczba '%d' jest %s\n",
-    n, jest_pierwsza ? "pierwsza" : "z³o¿ona");
+    n, jest_pierwsza ? "pierwsza" : "zÅ‚oÅ¼ona");
   return 0;
 }
