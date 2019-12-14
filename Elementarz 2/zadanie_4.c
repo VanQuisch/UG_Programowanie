8ded8da3abd010633a50b61612517d120f1f8017
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-14 09:08:44 +0100

    Update zadanie_4.c
    zamiana = na ==
    Przypisanie na porównanie

--- Elementarz 2/zadanie_4.c
+++ Elementarz 2/zadanie_4.c
@@ -8,11 +8,11 @@ int main() {
    nt = 0;  /* liczba znaków tabulacji */
    nl = 0;  /* liczba znaków nowego wiersza */
    while ((c=getchar()) != EOF) {
-      if(c=" ")     {nb++;};
-      if(c="    ")  {nt++;};
-      if(c="\n")    {nl++;};
+      if(c=='case:32')      {nb++;};
+      if(c=='case:09')      {nt++;};
+      if(c=='\n')           {nl++;};
    }
    printf("liczba znaków odstępu = %d, \
-      tabulacji = %d, nowego wiersza = %d\n", nb, nt, nl );  // wyrzuca kosmiczne wartości // Nie powinno tyle dodawać
+      tabulacji = %d, nowego wiersza = %d\n", nb, nt, nl);  // wyrzuca kosmiczne wartości // Nie powinno tyle dodawać
    return 0;
 }
