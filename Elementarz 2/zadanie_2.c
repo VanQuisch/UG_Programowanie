7f86a9b769b0f15fa5400883cd6ed9e5feaef533
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-14 08:11:10 +0100

    Update zadanie_2.c

--- Elementarz 2/zadanie_2.c
+++ Elementarz 2/zadanie_2.c
@@ -8,7 +8,7 @@ int main() {
   /* wczytaj tę liczbę do zmiennej n */
   scanf("%d", &n);
   /* sprawdź czy n jest liczbą pierwszą */
-  if (n%2==0 && n!=2){jest_pierwsza[0] = "pierwsza";}
+  if (n%2!=0 && n!=2){jest_pierwsza[0] = "pierwsza";}
   else {jest_pierwsza[0] = "złożona";};
   printf("Podana liczba '%d' jest %s\n",
     n, jest_pierwsza ? "pierwsza" : "złożona");
