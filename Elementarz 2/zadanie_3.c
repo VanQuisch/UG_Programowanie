2c455979db7ad3a47028002b4091d12ec1f08e78
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-14 08:45:36 +0100

    Update zadanie_3.c

--- Elementarz 2/zadanie_3.c
+++ Elementarz 2/zadanie_3.c
@@ -5,7 +5,7 @@ int j=0;
 
 int main()
 {
-    for(int i=0; i>; i+=2;)
+    for(int i=0; i>; i+=2;)  //nadal nie wiem co wstawić w for
     {j++;
         nxt_digit[j] = 4 - 4/i
         printf("Pi = %d", nxt_digit[j];);
