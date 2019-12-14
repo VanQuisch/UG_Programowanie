835c12ff0c61d1551b1ae9cdb65a21ba24400e0b
VanQuisch <56467790+VanQuisch@users.noreply.github.com>
2019-12-14 08:25:03 +0100

    Update zadanie_2.c
    komentarze

--- Elementarz 2/zadanie_2.c
+++ Elementarz 2/zadanie_2.c
@@ -8,9 +8,9 @@ int main() {
   /* wczytaj tę liczbę do zmiennej n */
   scanf("%d", &n);
   /* sprawdź czy n jest liczbą pierwszą */
-  if (n%2!=0 && n!=2){jest_pierwsza[0] = "pierwsza";}
-  else {jest_pierwsza[0] = "złożona";};
+  if (n%2!=0 && n!=2){jest_pierwsza[0] = "pierwsza";} //jeśli nie dzieli się na pół i jest różne od dwa
+  else {jest_pierwsza[0] = "złożona";};               //W każdym innym przypadku
   printf("Podana liczba '%d' jest %s\n",
-    n, jest_pierwsza ? "pierwsza" : "złożona");
+    n, jest_pierwsza ? "pierwsza" : "złożona");  //Nie rozumiem działania tego: jest_pierwsza ? "pierwsza" : "złożona"
   return 0;
 }
